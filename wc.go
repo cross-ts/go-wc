1d95007903dc5935a03367f377cf0664b57d3288
cross-ts <ts.kouhei@gmail.com>
2018-11-03 20:59:56 +0900

    wcの機能に関する実装をfunc wcに纏めた

--- wc.go
+++ wc.go
@@ -17,35 +17,32 @@ func main() {
 		wg.Add(1)
 		go func(fileName string) {
 			defer wg.Done()
-			file, err := os.Open(fileName)
-			defer file.Close()
-			if err != nil {
-				fmt.Printf("wc: %v: unknown file\n", fileName)
-				return
-			}
-			line, word, size, name := wc(file)
-			fmt.Printf("%v %v %v %v\n", line, word, size, name)
+			wc(fileName)
 		}(fileName)
 	}
 	wg.Wait()
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+func wc(fileName string) {
+	file, err := os.Open(fileName)
+	defer file.Close()
+	if err != nil {
+		fmt.Printf("wc: %v: unknown file\n", fileName)
+		return
 	}
-}
 
-func wc(file *os.File) (line int, word int, size int64, name string) {
 	stat, err := file.Stat()
-	check(err)
-	size, name = stat.Size(), stat.Name()
+	if err != nil {
+		panic(err)
+	}
+	size, name := stat.Size(), stat.Name()
 
 	scanner := bufio.NewScanner(file)
-	line, word = 0, 0
+	line, word := 0, 0
 	for scanner.Scan() {
 		line++
 		word += len(strings.Fields(scanner.Text()))
 	}
+	fmt.Printf("%v %v %v %v\n", line, word, size, name)
 	return
 }
